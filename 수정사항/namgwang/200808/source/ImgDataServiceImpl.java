5fd6ba0fa308b9bfaf1d1c4a2edb9b74568298fb
pieisland <35261724+pieisland@users.noreply.github.com>
2020-08-08 23:03:26 +0900

    Create ImgDataServiceImpl.java

---
+++ 수정사항/namgwang/200808/source/ImgDataServiceImpl.java
@@ -0,0 +1,49 @@
+package com.mme.realMulti.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mme.realMulti.dao.ImgDataDao;
+import com.mme.realMulti.dto.ImgData;
+import com.mme.realMulti.service.ImgDataService;
+
+@Service
+public class ImgDataServiceImpl implements ImgDataService{
+	@Autowired
+	ImgDataDao imgDataDao;
+	
+	@Override
+	@Transactional
+	public int updateMTInfoImg(ImgData imgdata) { //MT이미지 변경.
+		int ret = imgDataDao.update(imgdata);
+		return ret;
+	}
+	
+	@Override
+	@Transactional
+	public ImgData getImgName(String imgid) { //MT이미지 이름 가져오기.
+		return imgDataDao.getImgName(imgid);
+		//ImgData tmp = new ImgData();
+		//tmp = imgDataDao.getImgName(imgid);
+		//return tmp.getImgName();
+		//return imgname;
+	}
+	
+	@Override
+	@Transactional
+	public List<ImgData> getImgDatas(){
+		List<ImgData> list = imgDataDao.selectAll();
+		return list;
+	}
+	
+	//200808
+	@Override
+	@Transactional
+	public ImgData addImgData(ImgData img) {
+		imgDataDao.insert(img);
+		return img;
+	}	
+}
