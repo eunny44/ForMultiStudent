d82df9072e07d02e3da5b1684a7565f73b871c81
pieisland <35261724+pieisland@users.noreply.github.com>
2020-08-08 23:00:11 +0900

    Create Admin.java

---
+++ 수정사항/namgwang/200808/source/Admin.java
@@ -0,0 +1,57 @@
+package com.mme.realMulti.dto;
+import java.sql.Timestamp;
+
+public class Admin {
+	private String area;
+	private String title;
+	private String content;
+	private Timestamp startDate;
+	private Timestamp endDate;
+	
+	public String getArea() {
+		return area;
+	}
+	
+	public void setArea(String area) {
+		this.area = area;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Timestamp getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Timestamp startDate) {
+		this.startDate = startDate;
+	}
+
+	public Timestamp getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Timestamp endDate) {
+		this.endDate = endDate;
+	}	
+
+	//200808
+	@Override
+	public String toString() {
+		return "admin [area=" + area + ", title=" + title + ", content="
+				+ content + ", startDate=" + startDate + ", endDate=" + endDate +"]";
+	}
+}
